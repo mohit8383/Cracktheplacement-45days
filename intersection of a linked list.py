eb23c158d0a84d90a8d4648fcd518a9fc3777f9f
Mohit <111625817+mohit8383@users.noreply.github.com>
2024-07-26 22:19:33 +0530

    Create intersection of a linked list.py

---
+++ intersection of a linked list.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        one = headA
+        two = headB
+
+        while one != two:
+            one = headB if one is None else one.next
+            two = headA if two is None else two.next
+        return one
